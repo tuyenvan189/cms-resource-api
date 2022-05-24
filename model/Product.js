d0310303d0f189f1e9f85b35cd774bb1ddf39a29
vancattuyen92 <100502954+vancattuyen92@users.noreply.github.com>
2022-05-24 11:32:48 +0700

    Update Product.js

--- model/Product.js
+++ model/Product.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const userSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema({
   id:{
     type: String
   },
@@ -29,4 +29,4 @@ const userSchema = new mongoose.Schema({
   
 })
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('Product', productSchema)
