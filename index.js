e56250e160ca8ebd920682d08a75b9cad51157ac
vancattuyen92 <100502954+vancattuyen92@users.noreply.github.com>
2022-05-27 11:40:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get('/', (req, res) => {
 app.use(express.json({ extended: false }))
 app.use('/api/user', usersRoute)
 app.use('/api/photo', photosRoute)
-app.use('./api/product', productsRoute)
+app.use('/api/product', productsRoute)
 // app.use('/api/auth', authRoute)
 // app.use('/api/film', filmRoute)
 
